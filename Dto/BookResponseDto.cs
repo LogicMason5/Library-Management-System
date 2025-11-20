5ca2f0f4cebceae36c8665106677bf274b8b8abc
LogicMason5 <logicmason5@gmail.com>
2025-11-20 22:49:38 +0900

    Feat: update third.
    Co-authored-by: StackWeaver <stackweaver7@gmail.com>
    Co-authored-by: Snowstrom5 <snowmandot0520@gmail.com>
    Co-authored-by: FIFA0313 <footballplayer0313@gmail.com>

--- Dto/BookResponseDto.cs
+++ Dto/BookResponseDto.cs
@@ -7,8 +7,8 @@ namespace LibraryManagementSystemBackend.Dto
     {
 
         public Guid Id { get; set; }
-
         public required string Title { get; set; }
+
 
         public required string Author { get; set; }
 
