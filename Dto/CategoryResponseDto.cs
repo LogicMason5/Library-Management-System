f3af39611ed7cdd75399e8344bd40053d518eb6d
LogicMason5 <logicmason5@gmail.com>
2025-11-20 22:52:48 +0900

    Feat: update fourth. Co-authored-by: StackWeaver <stackweaver7@gmail.com> Co-authored-by: Snowstrom5 <snowmandot0520@gmail.com> Co-authored-by: FIFA0313 <footballplayer0313@gmail.com>

--- Dto/CategoryResponseDto.cs
+++ Dto/CategoryResponseDto.cs
@@ -5,14 +5,11 @@ namespace LibraryManagementSystemBackend.Dto
     public class CategoryResponseDto
     {
         public Guid Id { get; set; }
-
         public required string Name { get; set; }
-
         public string? Description { get; set; }
-
         public DateTime? CratedAt { get; set; } = DateTime.Now;
 
         public DateTime? UpdatedAt { get; set; } = DateTime.Now;
-        
+
     }
 }
